e742804b8e22389e38633000e5d8ea6743dadc00
ramprakash729 <raam.prakash729@gmail.com>
2021-06-09 20:41:37 +0530

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -42,7 +42,7 @@ export function addMovieToList(movie) {
 }
 
 export function handleMovieSearch(movie) {
-  const url = `http://www.omdbapi.com/?apikey=3ca5df7&t=${movie}`;
+  const url = `https://www.omdbapi.com/?apikey=3ca5df7&t=${movie}`;
   return function (dispatch) {
     fetch(url)
       .then((response) => response.json())
